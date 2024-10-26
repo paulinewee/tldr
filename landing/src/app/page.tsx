fef95aec1d5d47946d5a35088806415942ec9b22
Daniel Wei <weidaniel15@gmail.com>
2024-10-26 17:17:59 -0400

    Dark mode

--- landing/src/app/page.tsx
+++ landing/src/app/page.tsx
@@ -12,12 +12,20 @@ export default async function Home() {
         <main className='w-screen h-screen fixed flex flex-row items-center justify-center dark:bg-[#2f2525] text-[#3f2308] dark:text-[#FEF9F4] bg-[#FEF9F4]'>
             <div className='w-[400px] h-[640px] p-12 flex flex-col items-start justify-start select-none'>
                 <Image
+                    className='hidden dark:block'
+                    width={48}
+                    height={48}
+                    alt='Logo: Two people holding hands'
+                    src='/logo-white.png'
+                />
+                <Image
+                    className='dark:hidden'
                     width={48}
                     height={48}
                     alt='Logo: Two people holding hands'
                     src='/logo.png'
                 />
-                <h1 className='mt-2 text-3xl tracking-tighter no-select'>
+                <h1 className='mt-3 text-3xl tracking-tighter no-select'>
                     tldr
                 </h1>
                 <ul
@@ -48,8 +56,9 @@ export default async function Home() {
                         to grow and thrive.
                     </li>
                 </ul>
-                <button className={`${ApercuMono.className} text-sm relative`}>
-                  <CtaBtn />
+                <button className={`${ApercuMono.className} text-sm relative whitespace-nowrap`}>
+                  <CtaBtn dark={false} className='dark:hidden' />
+                  <CtaBtn dark={true} className='hidden dark:block' />
                   <span className='absolute top-3 left-3'>JOIN THE WAITLIST</span>
                 </button> 
             </div>
