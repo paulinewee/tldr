148e6ea7a9959d34825be21bcc0374ea2b1e8bd2
Daniel Wei <weidaniel15@gmail.com>
2024-10-26 16:12:14 -0400

    Push

--- landing/src/app/page.tsx
+++ landing/src/app/page.tsx
@@ -1,101 +1,58 @@
+import localFont from "next/font/local";
 import Image from "next/image";
+import Balancer from 'react-wrap-balancer';
+import CtaBtn from './icons/cta-btn';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const ApercuMono = localFont({
+    src: "./fonts/Apercu-Mono.otf",
+});
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+export default async function Home() {
+    return (
+        <main className='w-screen h-screen fixed flex flex-row items-center justify-center'>
+            <div className='w-[400px] h-[640px] p-12 flex flex-col items-start justify-start select-none text-[#3f2308]'>
+                <Image
+                    width={48}
+                    height={48}
+                    alt='Logo: Two people holding hands'
+                    src='/logo.png'
+                />
+                <h1 className='mt-2 text-3xl tracking-tighter no-select'>
+                    tldr
+                </h1>
+                <ul
+                    className={`flex flex-col tracking-tighter w-60 gap-4 ${ApercuMono.className} p-0 my-6`}
+                >
+                    <li className='text-xs p-0 m-0'>
+                        <span>love is context.</span>
+                        <br />
+                        we believe in places, spaces, and the power of context
+                        in fostering deep connection.
+                    </li>
+                    <li className='text-xs p-0 m-0'>
+                        <span>love compounds.</span>
+                        <br />
+                        we believe in collecting artifacts of affection, troves
+                        of tiny memories, galleries of memento.
+                    </li>
+                    <li className='text-xs p-0 m-0'>
+                        <span>love is consistent.</span>
+                        <br />
+                        it's not without friction, but it shouldn't come from
+                        the media of communication and exchange itself.
+                    </li>
+                    <li className='text-xs p-0 m-0'>
+                        <span>love coevolves.</span>
+                        <br />
+                        the best relationships are a foundation for both people
+                        to grow and thrive.
+                    </li>
+                </ul>
+                <button className={`${ApercuMono.className} text-sm relative hover:bg-[#3f2308]`}>
+                  <CtaBtn />
+                  <span className='absolute top-3 left-3'>JOIN THE WAITLIST</span>
+                </button> 
+            </div>
+        </main>
+    );
 }
